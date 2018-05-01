aaca4986443c7657dab1fc91f0b2918c5c432392
David Danko <dcdanko@gmail.com>
2018-05-01 12:49:54 -0400

    *args -> args

--- app/display_modules/ags/ags_tasks.py
+++ app/display_modules/ags/ags_tasks.py
@@ -31,7 +31,7 @@ def ags_distributions(samples):
 
 
 @celery.task()
-def reducer_task(*args):
+def reducer_task(args):
     """Combine AGS component calculations."""
     assert len(args) == 2, 'FOOBAR: ' + str(args)
     categories = args[0]
