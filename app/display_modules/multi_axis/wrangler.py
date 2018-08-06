47e65546f7cf7739be889f6d7aab7ed181476bca
David Danko <dcdanko@gmail.com>
2018-08-06 15:19:44 -0400

    fix persist task

--- app/display_modules/multi_axis/wrangler.py
+++ app/display_modules/multi_axis/wrangler.py
@@ -5,6 +5,7 @@ from celery import chord
 from app.display_modules.display_wrangler import DisplayModuleWrangler
 from app.display_modules.utils import categories_from_metadata
 
+from .constants import MODULE_NAME
 from .tasks import make_axes, persist_result
 
 
@@ -14,7 +15,7 @@ class MultiAxisWrangler(DisplayModuleWrangler):
     @classmethod
     def run_sample_group(cls, sample_group, samples):
         """Gather samples and process."""
-        persist_task = persist_result.s(sample_group.analysis_result_uuid)
+        persist_task = persist_result.s(sample_group.analysis_result_uuid, MODULE_NAME)
 
         categories_task = categories_from_metadata.s(samples)
         make_axes_task = make_axes.s(samples)
