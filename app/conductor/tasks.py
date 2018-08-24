fd58da30dd185c105467b46fdaef0d46cd14ae3e
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-08-24 14:09:25 -0400

    Add fetch_samples and filter_samples tasks with tests.

--- app/conductor/tasks.py
+++ app/conductor/tasks.py
@@ -1,7 +1,11 @@
 """Tasks for the Conductor module."""
 
+from flask import current_app
+
+from app.analysis_modules import all_analysis_modules
 from app.extensions import celery
 from app.sample_groups.sample_group_models import SampleGroup
+from app.tool_results import all_tool_results
 
 
 @celery.task()
@@ -10,3 +14,27 @@ def fetch_samples(sample_group_id):
     sample_group = SampleGroup.query.filter_by(id=sample_group_id).one()
     samples = [sample.fetch_safe() for sample in sample_group.samples]
     return samples
+
+
+@celery.task()
+def filter_samples(samples, module_name):
+    """Filter list of samples to only those supporting the given module."""
+    module = [module for module in all_analysis_modules
+              if module.name() is module_name][0]
+    dependencies = set([tool.name() for tool in module.required_tool_results()])
+
+    def test_sample(sample):
+        """Test a single sample to see if it has all tools required by the display module."""
+        all_fields = [mod.name() for mod in all_tool_results]
+        tools_present = set([field for field in all_fields
+                             if sample.get(field, None) is not None])
+        is_valid = dependencies <= tools_present
+        sample_name = sample['name']
+        current_app.logger.debug(f'Testing sample: {sample_name}')
+        current_app.logger.debug(f'Tools present: {tools_present}')
+        current_app.logger.debug(f'Is valid: {is_valid}')
+        return is_valid
+
+    result = [sample for sample in samples if test_sample(sample)]
+    current_app.logger.debug(f'result: {result}')
+    return result
---
+++ tests/conductor/test_tasks.py
@@ -0,0 +1,41 @@
+"""Test suite for Conductor tasks."""
+
+from analysis_packages.top_taxa import TopTaxaAnalysisModule
+from analysis_packages.top_taxa.tests.factory import create_values
+from tool_packages.krakenhll.constants import MODULE_NAME as KRAKENHLL_NAME
+
+from app.conductor.tasks import fetch_samples, filter_samples
+from app.extensions import db
+
+from tests.base import BaseTestCase
+from tests.utils import add_sample, add_sample_group
+
+
+class TestConductorTasks(BaseTestCase):
+    """Test suite for Conductor tasks."""
+
+    def test_fetch_samples(self):
+        """Ensure fetch_samples tasks works as expected."""
+        group = add_sample_group('Sample Group One', access_scheme='public')
+        group.samples = [add_sample(f'Sample {i}') for i in range(5)]
+        db.session.commit()
+
+        samples = fetch_samples.run(group.id)
+        self.assertEqual(len(samples), 5)
+
+    def test_filter_samples(self):
+        """Ensure filter_sample removes samples without required tool results."""
+        complete_values = create_values()
+        sample_complete = add_sample(name='Complete Sample',
+                                     metadata={'foo': f'bar'},
+                                     sample_kwargs=complete_values)
+        incomplete_values = create_values()
+        incomplete_values.pop(KRAKENHLL_NAME, None)
+        sample_incomplete = add_sample(name='Incomplete Sample',
+                                       metadata={'foo': f'bar'},
+                                       sample_kwargs=incomplete_values)
+        samples = [sample.fetch_safe() for sample in [sample_complete, sample_incomplete]]
+        module_name = TopTaxaAnalysisModule.name()
+        filtered_samples = filter_samples.run(samples, module_name)
+        self.assertEqual(len(filtered_samples), 1)
+        self.assertEqual(filtered_samples[0]['name'], 'Complete Sample')
