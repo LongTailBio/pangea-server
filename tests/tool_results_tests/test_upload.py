2dc5e2b6658a46d994b59d348a0c1c0ce1287d69
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-09-04 15:52:23 -0400

    Add support for GroupToolResult modules.

--- tests/tool_results_tests/test_upload.py
+++ tests/tool_results_tests/test_upload.py
@@ -1,6 +1,8 @@
 """Test suite for ToolResult modules API endpoints."""
 
 from app.tool_results.wrangler import all_tool_results
+
+from tool_packages.base import SampleToolResultModule
 from tool_packages.base.tests import get_result_module
 
 from .base import BaseToolResultTest
@@ -13,24 +15,29 @@ class TestToolResultUploads(BaseToolResultTest):
 
 
 for tool_result in all_tool_results:
-    module = tool_result
-    base_name = module.__name__
-    result_module = get_result_module(module)
+    base_name = tool_result.__name__
+    result_module = get_result_module(tool_result)
     module_name = result_module.name()
     current_factory = __import__(f'{base_name}.tests.factory', fromlist='dummy')
 
-    def add_module(self, name=module_name, factory=current_factory):
+    def add_module(self, module=result_module, name=module_name, factory=current_factory):
         """Ensure a ToolResult model is created correctly."""
         result = factory.create_result()
-        self.generic_add_sample_tool_test(result, name)
+        if issubclass(module, (SampleToolResultModule,)):
+            self.generic_add_sample_tool_test(result, name)
+        else:
+            self.generic_add_group_tool_test(result, module.result_model())
 
     add_module.__doc__ = f'Ensure a raw {module.__name__} model is created correctly.'
     setattr(TestToolResultUploads, f'test_add_{module_name}', add_module)
 
-    def upload_module(self, name=module_name, factory=current_factory):
+    def upload_module(self, module=result_module, name=module_name, factory=current_factory):
         """Ensure a raw ToolResult can be uploaded."""
         payload = factory.create_values()
-        self.generic_test_upload_sample(payload, name)
+        if issubclass(module, (SampleToolResultModule,)):
+            self.generic_test_upload_sample(payload, name)
+        else:
+            self.generic_test_upload_group(module.result_model(), payload, name)
 
     upload_module.__doc__ = f'Ensure a raw {module.__name__} can be uploaded.'
     setattr(TestToolResultUploads, f'test_upload_{module_name}', upload_module)
