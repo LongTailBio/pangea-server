90aed8de0d4d1d44f769d7c4e3f3478be6e4cf95
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-09-11 12:40:59 -0400

    Generate middleware tests for all modules.

--- tests/analysis_modules/test_middleware.py
+++ tests/analysis_modules/test_middleware.py
@@ -1,29 +1,30 @@
 """Test running middleware fully."""
 
-from analysis_packages.ags import AGSAnalysisModule
 from analysis_packages.base.exceptions import UnsupportedAnalysisMode
 
+from app.analysis_modules.wrangler import all_analysis_modules
 from app.analysis_modules.utils import conduct_sample, conduct_sample_group
 from app.extensions import db
 
 from ..tool_results.utils import unpack_module as unpack_tool
 from ..utils import add_sample_group, add_sample
 from .base import BaseAnalysisModuleTest
+from .utils import unpack_module
 
 
-def processes_samples(module):
+def processes_samples(analysis_module):
     """Return true if a module processes SampleToolResults."""
     try:
         # pylint: disable=assignment-from-no-return
-        _ = module.group_tool_processor()
+        _ = analysis_module.group_tool_processor()
         return False
     except UnsupportedAnalysisMode:
         return True
 
 
-def seed_samples(module, samples):
+def seed_samples(analysis_module, samples):
     """Create single sample."""
-    tool_modules = module.required_tool_results()
+    tool_modules = analysis_module.required_tool_results()
     for tool in tool_modules:
         name = tool.name()
         factory = unpack_tool(tool)[2]
@@ -39,14 +40,14 @@ def numbered_sample(i):
     return sample
 
 
-def seed_module(module, sample_group, samples):
+def seed_module(analysis_module, sample_group, samples):
     """Seed testing values for moduke."""
-    if processes_samples(module):
+    if processes_samples(analysis_module):
         # Prepate Samples with ToolResults, if applicable
-        seed_samples(module, samples)
+        seed_samples(analysis_module, samples)
     else:
         # Prepare GroupToolResult(s), if applicable
-        factory = unpack_tool(module)[2]
+        factory = unpack_tool(analysis_module)[2]
         tool_result = factory.create_result(save=False)
         tool_result.sample_group_uuid = sample_group.id
         tool_result.save()
@@ -55,10 +56,15 @@ def seed_module(module, sample_group, samples):
 class TestAnalysisModuleMiddleware(BaseAnalysisModuleTest):
     """Test running middleware fully."""
 
-    def test_single_sample(self):
-        """Test middleware for single Sample analyses."""
-        analysis_module = AGSAnalysisModule
+    pass
 
+
+for module in all_analysis_modules:
+    # Grab top-level values we need
+    analysis_name = unpack_module(module)[1]
+
+    def single_sample_test(self, analysis_module=module):
+        """Test middleware for single Sample analyses."""
         sample = numbered_sample(0)
         seed_samples(analysis_module, [sample])
         module_name = analysis_module.name()
@@ -67,12 +73,18 @@ class TestAnalysisModuleMiddleware(BaseAnalysisModuleTest):
         ags_task()
 
         analysis_result = sample.analysis_result.fetch()
-        self.assertNotIn(module_name, analysis_result)
+        try:
+            _ = analysis_module.single_sample_processor()
+            self.assertIn(module_name, analysis_result)
+        except UnsupportedAnalysisMode:
+            self.assertNotIn(module_name, analysis_result)
 
-    def test_sample_group(self):
+    single_sample_test.__doc__ = f'Test {analysis_name} middleware for single Sample.'
+    test_name = f'test_{analysis_name}_single_sample'
+    setattr(TestAnalysisModuleMiddleware, test_name, single_sample_test)
+
+    def sample_group_test(self, analysis_module=module):
         """Test middleware for SampleGroup analyses."""
-        analysis_module = AGSAnalysisModule
-
         # Seed test values
         sample_group = add_sample_group('Test Group')
         samples = [numbered_sample(i) for i in range(5)]
@@ -94,3 +106,7 @@ class TestAnalysisModuleMiddleware(BaseAnalysisModuleTest):
             result = model.objects.get(sample_group_uuid=sample_group.id)
         self.assertEqual(result.status, 'S')
         self.assertIn('data', result)
+
+    sample_group_test.__doc__ = f'Test {analysis_name} middleware SampleGroup.'
+    test_name = f'test_{analysis_name}_sample_group'
+    setattr(TestAnalysisModuleMiddleware, test_name, sample_group_test)
