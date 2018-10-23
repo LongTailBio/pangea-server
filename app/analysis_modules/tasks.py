397d359214697afe9484ecf28e668afd180cac51
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-10-22 21:35:24 -0700

    Use PluginTask.

--- app/analysis_modules/tasks.py
+++ app/analysis_modules/tasks.py
@@ -1,23 +1,13 @@
 """Tasks for the Conductor module."""
 
-from app.analysis_results.analysis_result_models import AnalysisResultMeta
-from app.extensions import celery
+from celery import Task
+
+from app.extensions import celery_logger
 
 
-@celery.task(bind=True)
-def clean_error(self, uuid):
-    """
-    Handle expected error types cleanly.
+class PluginTask(Task):  # pylint: disable=abstract-method
+    """Base task for running plugin processors."""
 
-    To be used like: module_task.s(sample_group_id).on_error(clean_error.s()).delay()
-    """
-    # Try to get analysis_result.id from parent tasks metadata
-    result = AsyncResult(uuid)
-    parent_meta = result.info
-    analysis_result_id = parent_meta['analysis_result_id']
-    module_name = parent_meta['module_name']
-    analysis_result = AnalysisResultMeta.objects.get(uuid=analysis_result_id)
-    wrapper = getattr(analysis_result, module_name).fetch()
-    wrapper.status = 'E'
-    wrapper.save()
-    analysis_result.save()
+    def on_failure(self, exc, task_id, args, kwargs, einfo):
+        """Log error details."""
+        celery_logger.warn('Arguments: %s', args)
--- app/analysis_modules/utils.py
+++ app/analysis_modules/utils.py
@@ -16,6 +16,8 @@ from app.sample_groups.sample_group_models import SampleGroup
 from app.tool_results import all_tool_results
 from app.utils import lock_function
 
+from .tasks import PluginTask
+
 
 def apply_errback(signatures):
     """Add error callback to list of signatures."""
@@ -101,7 +103,7 @@ def task_body_sample(sample_uuid, module):
     persist_result_helper(analysis_result, module, data)
 
 
-@celery.task()
+@celery.task(base=PluginTask)
 def run_sample(sample_uuid, module_name):
     """Wrap analysis work for single Sample."""
     try:
@@ -153,7 +155,7 @@ def task_body_group_tool_result(sample_group_uuid, module):
     persist_result_helper(analysis_result, module, data)
 
 
-@celery.task()
+@celery.task(base=PluginTask)
 def run_sample_group(sample_group_uuid, module_name):
     """Run middleware for a sample group."""
     try:
