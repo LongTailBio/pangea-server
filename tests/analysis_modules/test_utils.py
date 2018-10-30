53c35642e02b54f02dbe31b43752e8320b708c89
David Danko <dcdanko@gmail.com>
2018-10-30 10:54:18 -0400

    remove filter sample test, since samples are no longer filtered

--- tests/analysis_modules/test_utils.py
+++ tests/analysis_modules/test_utils.py
@@ -22,20 +22,3 @@ class TestConductorTasks(BaseTestCase):
 
         samples = fetch_samples(group.id)
         self.assertEqual(len(samples), 5)
-
-    def test_filter_samples(self):
-        """Ensure filter_sample removes samples without required tool results."""
-        complete_values = create_values()
-        sample_complete = add_sample(name='Complete Sample',
-                                     metadata={'foo': f'bar'},
-                                     sample_kwargs=complete_values)
-        incomplete_values = create_values()
-        incomplete_values.pop(KRAKENHLL_NAME, None)
-        sample_incomplete = add_sample(name='Incomplete Sample',
-                                       metadata={'foo': f'bar'},
-                                       sample_kwargs=incomplete_values)
-        samples = [sample_complete, sample_incomplete]
-        module = TopTaxaAnalysisModule
-        filtered_samples = filter_samples(samples, module)
-        self.assertEqual(len(filtered_samples), 1)
-        self.assertEqual(filtered_samples[0]['name'], 'Complete Sample')
