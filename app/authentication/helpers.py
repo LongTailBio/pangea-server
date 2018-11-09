3a74d77ad7514daffa3db48ba19f25499563d0d8
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-11-09 10:14:27 -0500

    Add new authentication models.

---
+++ app/authentication/helpers.py
@@ -0,0 +1,69 @@
+"""Helper methods related to User model."""
+
+from functools import wraps
+
+import datetime
+import uuid
+import jwt
+
+from flask import request, current_app
+from flask_api.exceptions import NotAuthenticated, AuthenticationFailed
+
+from app.authentication.models import User
+
+
+def encode_auth_token(user_uuid):
+    """Generate the auth token."""
+    try:
+        days = current_app.config.get('TOKEN_EXPIRATION_DAYS')
+        seconds = current_app.config.get('TOKEN_EXPIRATION_SECONDS')
+        payload = {
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(
+                days=days, seconds=seconds),
+            'iat': datetime.datetime.utcnow(),
+            'sub': str(user_uuid)
+        }
+        return jwt.encode(
+            payload,
+            current_app.config.get('SECRET_KEY'),
+            algorithm='HS512',
+        )
+    except Exception as exc:  # pylint: disable=broad-except
+        return exc
+
+
+def decode_auth_token(auth_token):
+    """Decode the auth token - :param auth_token: - :return: UUID|string."""
+    try:
+        secret = current_app.config.get('SECRET_KEY')
+        payload = jwt.decode(auth_token, secret, algorithms=['HS512'])
+        return uuid.UUID(payload['sub'])
+    except jwt.ExpiredSignatureError:
+        raise AuthenticationFailed('Signature expired. Please log in again.')
+    except jwt.InvalidTokenError:
+        raise AuthenticationFailed('Invalid token. Please log in again.')
+
+
+def authenticate(required=True):
+    """Decorate API route calls requiring authentication."""
+    def wrapper(f):  # pylint: disable=invalid-name
+        """Wrap decorated function."""
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            """Wrap function f."""
+            try:
+                auth_header = request.headers.get('Authorization')
+                if not auth_header:
+                    raise NotAuthenticated('Provide a valid auth token.')
+                auth_token = auth_header.split(' ')[1]
+                user_uuid = decode_auth_token(auth_token)
+                user = User.query.filter_by(uuid=user_uuid).first()
+                if not user or not user.active:
+                    raise AuthenticationFailed('User is not active')
+                return f(user_uuid, *args, **kwargs)
+            except (NotAuthenticated, AuthenticationFailed):
+                if required:
+                    raise
+                return f(None, *args, **kwargs)
+        return decorated_function
+    return wrapper
---
+++ app/authentication/models.py
@@ -0,0 +1,134 @@
+"""User model definitions."""
+
+# pylint: disable=too-many-arguments,too-few-public-methods
+
+import datetime
+
+from flask import current_app
+from sqlalchemy.dialects.postgresql import UUID, ENUM
+from sqlalchemy.ext.associationproxy import association_proxy
+from marshmallow import fields
+
+from app.base import BaseSchema
+from app.extensions import db, bcrypt
+
+
+class OrganizationMembership(db.Model):
+    """Association object for linking users to organizations with role."""
+
+    __tablename__ = 'organization_memberships'
+
+    organization_uuid = db.Column(UUID(as_uuid=True), db.ForeignKey('users.uuid'),
+                                  primary_key=True, nullable=False)
+    user_uuid = db.Column(UUID(as_uuid=True), db.ForeignKey('users.uuid'),
+                          primary_key=True, nullable=False)
+    role = db.Column(ENUM('admin', 'write' 'read', name='organization_role'),
+                     nullable=False)
+    is_public = db.Column(db.Boolean, default=True, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+
+    __table_args__ = (
+        db.UniqueConstraint('organization_uuid', 'user_uuid', name='_organization_user_uc'),
+    )
+
+    def __init__(
+            self, organization_uuid, user_uuid, role,
+            is_public=True, created_at=datetime.datetime.utcnow()):
+        self.organization_uuid = organization_uuid
+        self.user_uuid = user_uuid
+        self.role = role
+        self.is_public = is_public
+        self.created_at = created_at
+
+
+class User(db.Model):
+    """Pangea User model.
+
+    To make ownership of sample libraries easier, users and organizations
+    are treated as the same entity.
+    """
+
+    __tablename__ = 'users'
+
+    uuid = db.Column(UUID(as_uuid=True),
+                     primary_key=True,
+                     server_default=db.text('uuid_generate_v4()'))
+    username = db.Column(db.String(128), unique=True, nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    user_type = db.Column(ENUM('user', 'organization', name='user_type'),
+                          nullable=False)
+    is_deleted = db.Column(db.Boolean, default=False, nullable=False)
+    is_fake = db.Column(db.Boolean, default=False, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+
+    # Users that belong to this organization
+    user_memberships = db.relationship(
+        'OrganizationMembership',
+        primaryjoin='User.uuid == OrganizationMembership.organization_uuid',
+        backref=db.backref('organization'),
+    )
+    users = association_proxy('user_memberships', 'user')
+
+    # Organizations this user belongs to
+    organization_memberships = db.relationship(
+        'OrganizationMembership',
+        primaryjoin='User.uuid == OrganizationMembership.user_uuid',
+        backref=db.backref('user'),
+    )
+    organizations = association_proxy('organization_memberships', 'organization')
+
+    def __init__(
+            self, username, email, user_type,
+            is_deleted=False, is_fake=False,
+            created_at=datetime.datetime.utcnow()):
+        """Initialize MetaGenScope User model."""
+        self.username = username
+        self.email = email
+        self.user_type = user_type
+        self.is_deleted = is_deleted
+        self.is_fake = is_fake
+        self.created_at = created_at
+
+
+class PasswordAuthentication(db.Model):
+    """Password Authentication model.
+
+    Pangea will support multiple authentication modes, each stored separately.
+    """
+
+    __tablename__ = 'password_authentication'
+
+    user_uuid = db.Column(UUID(as_uuid=True), db.ForeignKey('users.uuid'),
+                          primary_key=True, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+
+    # One-to-one relationship with User
+    user = db.relationship('User', backref=db.backref('password_authentication', uselist=False))
+
+    def __init__(
+            self, user_uuid, password,
+            created_at=datetime.datetime.utcnow()):
+        """Initialize MetaGenScope User model."""
+        self.user_uuid = user_uuid
+        self.password = bcrypt.generate_password_hash(
+            password, current_app.config.get('BCRYPT_LOG_ROUNDS')
+        ).decode()
+        self.created_at = created_at
+
+
+class UserSchema(BaseSchema):
+    """Serializer for User."""
+
+    __envelope__ = {
+        'single': 'user',
+        'many': 'users',
+    }
+    __model__ = User
+
+    uuid = fields.Str()
+    username = fields.Str()
+    email = fields.Str()
+
+
+user_schema = UserSchema()      # pylint: disable=invalid-name
