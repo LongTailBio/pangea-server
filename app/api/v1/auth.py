6b6da4a1bf236b5836de3b0fef24b249ad51f9e9
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-11-09 10:14:44 -0500

    Update auth API.

--- app/api/v1/auth.py
+++ app/api/v1/auth.py
@@ -1,14 +1,28 @@
 """Authentication API endpoint definitions."""
 
+# Required model arguments are set via relationships
+# pylint: disable=no-value-for-parameter
+
+from uuid import UUID
+
 from flask import Blueprint, current_app, request
-from flask_api.exceptions import ParseError, NotFound
-from sqlalchemy import or_
+from flask_api.exceptions import ParseError, NotFound, PermissionDenied
+from sqlalchemy import or_, asc
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm.exc import NoResultFound
 
 from app.api.exceptions import InvalidRequest, InternalError
 from app.extensions import db, bcrypt
-from app.users.user_models import User
-from app.users.user_helpers import authenticate
+from app.authentication.models import (
+    User,
+    OrganizationMembership,
+    PasswordAuthentication,
+    user_schema,
+)
+from app.authentication.helpers import encode_auth_token, authenticate
+
+
+PAGE_SIZE = 20
 
 
 auth_blueprint = Blueprint('auth', __name__)  # pylint: disable=invalid-name
@@ -38,8 +52,9 @@ def register_user():
         new_user = User(
             username=username,
             email=email,
-            password=password,
+            user_type='user',
         )
+        new_user.password_authentication = PasswordAuthentication(password=password)
         db.session.add(new_user)
         db.session.commit()
     except IntegrityError as integrity_error:
@@ -48,7 +63,7 @@ def register_user():
         raise InternalError(str(integrity_error))
 
     # Generate auth token
-    auth_token = new_user.encode_auth_token(new_user.id)
+    auth_token = encode_auth_token(new_user.uuid)
     result = {'auth_token': auth_token.decode()}
     return result, 201
 
@@ -67,8 +82,10 @@ def login_user():
 
     # Fetch the user data
     user = User.query.filter_by(email=email).first()
-    if user and bcrypt.check_password_hash(user.password, password):
-        auth_token = user.encode_auth_token(user.id)
+    password_authentication = getattr(user, 'password_authentication', None)
+    password_hash = getattr(password_authentication, 'password', None)
+    if password_hash and bcrypt.check_password_hash(password_hash, password):
+        auth_token = encode_auth_token(user.uuid)
         if auth_token:
             result = {'auth_token': auth_token.decode()}
             return result, 200
@@ -77,16 +94,16 @@ def login_user():
 
 @auth_blueprint.route('/auth/logout', methods=['GET'])
 @authenticate()
-def logout_user(resp):  # pylint: disable=unused-argument
+def logout_user(authn_uuid):  # pylint: disable=unused-argument
     """Log user out."""
     return {}, 200
 
 
 @auth_blueprint.route('/auth/status', methods=['GET'])
 @authenticate()
-def get_user_status(resp):
+def get_user_status(authn_uuid):
     """Get user status."""
-    user = User.query.filter_by(id=resp).first()
+    user = User.query.filter_by(uuid=authn_uuid).first()
     result = {
         'id': str(user.id),
         'username': user.username,
@@ -95,3 +112,165 @@ def get_user_status(resp):
         'created_at': user.created_at
     }
     return result, 200
+
+
+@auth_blueprint.route('/organizations', methods=['POST'])
+@authenticate()
+def add_organization(authn_uuid):
+    """Add organization."""
+    try:
+        post_data = request.get_json()
+        username = post_data['username']
+        email = post_data['email']
+    except TypeError:
+        raise ParseError('Missing organization payload.')
+    except KeyError:
+        raise ParseError('Invalid organization payload.')
+
+    organization = User.query.filter_by(username=username).first()
+    if organization is not None:
+        raise InvalidRequest('That name is in use.')
+
+    organization = User.query.filter_by(email=email).first()
+    if organization is not None:
+        raise InvalidRequest('That email is in use.')
+
+    authn_user = User.query.filter_by(uuid=authn_uuid).one()
+    try:
+        membership = OrganizationMembership(role='admin')
+        membership.user = authn_user
+        organization = User(username=username, email=email, user_type='organization')
+        organization.user_memberships.append(membership)
+        db.session.add_all([organization, membership])
+        db.session.commit()
+
+        result = user_schema.dump(organization)
+        return result, 201
+    except IntegrityError as integrity_error:
+        current_app.logger.exception('There was a problem adding an organization.')
+        db.session.rollback()
+        raise InternalError(str(integrity_error))
+
+
+@auth_blueprint.route('/organizations', methods=['GET'])
+def get_organizations():
+    """Get all organizations."""
+    if 'name' in request.args:
+        name_query = request.args.get('name')
+        try:
+            organization = User.query.filter_by(user_type='organization',
+                                                username=name_query).one()
+        except NoResultFound:
+            raise NotFound('Organization does not exist')
+        result = user_schema.dump(organization)
+        return result, 200
+
+    limit = request.args.get('limit', PAGE_SIZE)
+    offset = request.args.get('offset', 0)
+    organizations = User.query.filter_by(user_type='organization') \
+        .limit(limit) \
+        .offset(offset) \
+        .order_by(asc(User.created_at)) \
+        .all()
+
+    result = user_schema.dump(organizations, many=True)
+    return result, 200
+
+
+@auth_blueprint.route('/organizations/<organization_uuid>', methods=['GET'])
+def get_single_organization(organization_uuid):
+    """Get single organization details."""
+    try:
+        organization_uuid = UUID(organization_uuid)
+    except ValueError:
+        raise ParseError('Invalid organization UUID.')
+
+    try:
+        organization = User.query.filter_by(uuid=organization_uuid).one()
+    except NoResultFound:
+        raise NotFound('Organization does not exist')
+
+    result = user_schema.dump(organization)
+    return result, 200
+
+
+@auth_blueprint.route('/organizations/<organization_uuid>/users', methods=['GET'])
+@authenticate(required=False)
+def get_organization_users(authn_uuid, organization_uuid):
+    """Get single organization's users."""
+    try:
+        organization_uuid = UUID(organization_uuid)
+    except ValueError:
+        raise ParseError('Invalid organization UUID.')
+
+    try:
+        organization = User.query.filter_by(uuid=organization_uuid).one()
+    except NoResultFound:
+        raise NotFound('Organization does not exist')
+
+    authn_user = User.query.filter_by(uuid=authn_uuid).one() if authn_uuid else None
+    if authn_user in organization.users:
+        result = user_schema.dump(organization.users, many=True)
+        return result, 200
+
+    users = organization.users.filter_by(is_public=True).all()
+    result = user_schema.dump(users, many=True)
+    return result, 200
+
+@auth_blueprint.route('/organizations/<organization_uuid>/users', methods=['POST'])
+@authenticate()
+def add_organization_user(authn_uuid, organization_uuid):     # pylint: disable=too-many-return-statements
+    """Add user to organization."""
+    try:
+        post_data = request.get_json()
+        user_uuid = post_data['user_uuid']
+        organization_uuid = UUID(organization_uuid)
+    except TypeError:
+        raise ParseError('Missing membership payload.')
+    except KeyError:
+        raise ParseError('Invalid membership payload.')
+    except ValueError:
+        raise ParseError('Invalid organization UUID.')
+
+    try:
+        organization = User.query.filter_by(uuid=organization_uuid).one()
+    except NoResultFound:
+        raise NotFound('Organization does not exist')
+
+    authn_user = User.query.filter_by(uuid=authn_uuid).one()
+    try:
+        _ = OrganizationMembership.query.filter_by(
+            organization_uuid=organization.uuid,
+            user_uuid=authn_user.uuid,
+            role='admin',
+        ).one()
+    except NoResultFound:
+        message = 'You do not have permission to add a user to that organization.'
+        raise PermissionDenied(message)
+
+    try:
+        user = User.query.filter_by(uuid=user_uuid).one()
+    except NoResultFound:
+        raise InvalidRequest('User does not exist')
+
+    membership = OrganizationMembership.query.filter_by(
+        organization_uuid=organization.uuid,
+        user_uuid=user.uuid,
+    ).first()
+    if membership:
+        raise InvalidRequest('User is already part of organization.')
+
+    try:
+        role = post_data.get('role', 'read')
+        membership = OrganizationMembership(role=role)
+        membership.user = user
+        membership.organization = organization
+        db.session.add(membership)
+        db.session.commit()
+        message = f'${user.username} added to ${organization.name}'
+        result = {'message': message}
+        return result, 200
+    except IntegrityError as integrity_error:
+        current_app.logger.exception('IntegrityError encountered.')
+        db.session.rollback()
+        raise InternalError(str(integrity_error))
