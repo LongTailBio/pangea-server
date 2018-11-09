b89b6541239e13975c1bc2ad7d5ced77090ec3e2
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-11-09 12:57:09 -0500

    Remove deprecated files.

--- app/__init__.py
+++ app/__init__.py
@@ -14,11 +14,9 @@ from app.analysis_modules.register import register_display_module
 from app.api.constants import URL_PREFIX
 from app.api.v1.analysis_results import analysis_results_blueprint
 from app.api.v1.auth import auth_blueprint
-from app.api.v1.organizations import organizations_blueprint
 from app.api.v1.ping import ping_blueprint
 from app.api.v1.samples import samples_blueprint
 from app.api.v1.sample_groups import sample_groups_blueprint
-from app.api.v1.users import users_blueprint
 from app.config import app_config
 from app.extensions import mongoDB, db, migrate, bcrypt, celery
 
@@ -64,11 +62,9 @@ def register_blueprints(app):
     """Register API endpoint blueprints for app."""
     app.register_blueprint(analysis_results_blueprint, url_prefix=URL_PREFIX)
     app.register_blueprint(auth_blueprint, url_prefix=URL_PREFIX)
-    app.register_blueprint(organizations_blueprint, url_prefix=URL_PREFIX)
     app.register_blueprint(ping_blueprint, url_prefix=URL_PREFIX)
     app.register_blueprint(samples_blueprint, url_prefix=URL_PREFIX)
     app.register_blueprint(sample_groups_blueprint, url_prefix=URL_PREFIX)
-    app.register_blueprint(users_blueprint, url_prefix=URL_PREFIX)
 
 
 def register_error_handlers(app):
